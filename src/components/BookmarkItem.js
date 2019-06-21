f94e2734d55feda8323125c1f25eb910648e9e6b
Valerio <valeriocriscuolo91@gmail.com>
2019-06-21 09:59:50 +0100

    fixed bug with url input

--- src/components/BookmarkItem.js
+++ src/components/BookmarkItem.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 import '../styles/BookmarkItem.css';
 
-const BookmarkItem = ({ bookmark }) => (
-  <ul className="bookmark-info">
-    <div className="title-container">
-      <div className="title">{bookmark.title}</div>
-    </div>
-    <div className="link-container">
-      <a href={bookmark.link} className="link">{bookmark.link}</a>
-    </div>
-  </ul>
-);
+const BookmarkItem = ({ bookmark }) => {
+  const { link } = bookmark;
+  const bookmarklink = link.replace(/\s+/g, '');
+
+
+  return (
+    <ul className="bookmark-info">
+      <div className="title-container">
+        <div className="title">{bookmark.title}</div>
+      </div>
+      <div className="link-container">
+        <a href={bookmarklink} className="link">{bookmarklink}</a>
+      </div>
+    </ul>
+  );
+};
 
 export default BookmarkItem;
